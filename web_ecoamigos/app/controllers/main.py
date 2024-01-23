f14c754e47ce23a6d6923521bdff72fd0789c538
Estevan Gutierrez <estevangutierrezzarate@gmail.com>
2024-01-23 16:53:06 -0500

    Render blog

--- web_ecoamigos/app/controllers/main.py
+++ web_ecoamigos/app/controllers/main.py
@@ -5,3 +5,7 @@ main = Blueprint('main',__name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/blog')
+def blog():
+    return render_template('blog.html')
